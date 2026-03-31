7e549a89c1ddbfefc39e18cf831b5d645dc6a87d
dikidjatar <dikidjatar@gmail.com>
2026-04-01 06:48:00 +0700

    feat: add new builtin string methods `ganti`, `pisahin`, `berisi`

--- src/builtins/string-methods.ts
+++ src/builtins/string-methods.ts
@@ -79,5 +79,36 @@ define(
   },
   [required("indeks")]
 );
+define(
+  "ganti",
+  (value, args) => {
+    const searchValue = args[0];
+    const replaceValue = args[1];
+    const newValue = value.replace(
+      searchValue.inspect(),
+      replaceValue.inspect()
+    );
+    return Factory.NewString(newValue);
+  },
+  [required("teks"), required("ganti")]
+);
+define(
+  "pisahin",
+  (value, args) => {
+    const limit = args[1].isNumber() ? args[1].value : undefined;
+    const elements = value.split(args[0].inspect(), limit);
+    return Factory.NewList(elements.map((e) => Factory.NewString(e)));
+  },
+  [required("pemisah"), optional("batas", Factory.NewNumber(-1))]
+);
+define(
+  "berisi",
+  (value, args) => {
+    const searchString = args[0];
+    const position = args[1].isNumber() ? args[1].value : undefined;
+    return Factory.Boolean(value.includes(searchString.inspect(), position));
+  },
+  [required("teks"), optional("posisi", Factory.Kosong)]
+);
 
 stringMethod.registerFor(SantaiType.kString);
