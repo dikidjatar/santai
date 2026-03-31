fdc74d69a5a4f06964ae009444279dae86b52812
dikidjatar <dikidjatar@gmail.com>
2026-03-31 17:37:05 +0700

    feat: add named arguments for list builtin methods

--- src/builtins/list-methods.ts
+++ src/builtins/list-methods.ts
@@ -6,35 +6,49 @@ import { Factory, SantaiObject } from "../objects/object";
 import { SantaiType } from "../objects/st-type";
 import { arg0 } from "./builtin";
 import { MethodTable } from "./methods";
+import { BuiltinParam, required } from "./paramSpec";
 
 const listMethods = new MethodTable();
 
 function define(
   name: string,
-  fn: (value: SantaiObject[], args: SantaiObject[]) => SantaiObject
+  fn: (value: SantaiObject[], args: SantaiObject[]) => SantaiObject,
+  params?: readonly BuiltinParam[]
 ) {
-  listMethods.define(name, (self, args) => {
-    assertDefined(self);
-    assert(self.isList());
+  listMethods.define(
+    name,
+    (self, args) => {
+      assertDefined(self);
+      assert(self.isList());
 
-    return fn(self.elements, args);
-  });
+      return fn(self.elements, args);
+    },
+    params
+  );
 }
 
-define("tambah", (elements, args) => {
-  for (const arg of args) elements.push(arg);
-  return Factory.NewNumber(elements.length);
-});
+define(
+  "tambah",
+  (elements, args) => {
+    for (const arg of args) elements.push(arg);
+    return Factory.NewNumber(elements.length);
+  },
+  [required("data")]
+);
 
-define("hapus", (elements, args) => {
-  const arg = arg0(args);
-  if (arg.isNumber()) {
-    const index = arg.value;
-    if (index >= 0 && index < elements.length) {
-      elements.splice(index, 1);
+define(
+  "hapus",
+  (elements, args) => {
+    const arg = arg0(args);
+    if (arg.isNumber()) {
+      const index = arg.value;
+      if (index >= 0 && index < elements.length) {
+        elements.splice(index, 1);
+      }
     }
-  }
-  return Factory.Kosong;
-});
+    return Factory.Kosong;
+  },
+  [required("indeks")]
+);
 
 listMethods.registerFor(SantaiType.kList);
