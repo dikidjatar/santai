4a7521797dd0470799d63c0e253d459f72277eca
dikidjatar <dikidjatar@gmail.com>
2026-04-07 12:19:50 +0700

    feat: change the type registry id and adjust the system builtins

--- src/objects/typeRegistry.ts
+++ src/objects/typeRegistry.ts
@@ -1,24 +1,27 @@
 // Copyright (c) [2026] [Diki Djatar]
 // SPDX-License-Identifier: MIT
 
-import { SantaiBuiltinClass, SantaiObject } from "./object";
-import { SantaiType } from "./st-type";
+import { SantaiObject } from "./object";
 
 export interface ITypeRegistry {
-  registerType(cls: SantaiBuiltinClass): SantaiBuiltinClass;
-  getTypeOf(obj: SantaiObject): SantaiBuiltinClass | undefined;
+  registerType<T extends SantaiObject>(obj: T): T;
+  getTypeOf(obj: SantaiObject): SantaiObject | undefined;
 }
 
 class TypeRegistryImpl implements ITypeRegistry {
-  private readonly _types = new Map<SantaiType, SantaiBuiltinClass>();
+  private readonly _types = new Map<string, SantaiObject>();
 
-  registerType(cls: SantaiBuiltinClass): SantaiBuiltinClass {
-    this._types.set(cls.santaiType, cls);
-    return cls;
+  registerType<T extends SantaiObject>(obj: T): T {
+    this._types.set(this.id(obj), obj);
+    return obj;
   }
 
-  getTypeOf(obj: SantaiObject): SantaiBuiltinClass | undefined {
-    return this._types.get(obj.type);
+  getTypeOf(obj: SantaiObject): SantaiObject | undefined {
+    return this._types.get(this.id(obj));
+  }
+
+  private id(obj: SantaiObject): string {
+    return `${obj.isBuiltinClass() ? obj.santaiType : obj.type}-${obj.typeName}`;
   }
 }
 
