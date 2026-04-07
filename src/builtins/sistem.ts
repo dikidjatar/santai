563f8b5c703be7f8f3db6b0fb300c583e9dd097a
dikidjatar <dikidjatar@gmail.com>
2026-04-07 13:18:40 +0700

    refactor: update builtin type sistem and env

--- src/builtins/sistem.ts
+++ src/builtins/sistem.ts
@@ -5,7 +5,6 @@ import * as meta from "../base/meta";
 import { isUndefined } from "../base/types";
 import { Factory, SantaiObject } from "../objects/object";
 import { SantaiType } from "../objects/st-type";
-import { TypeRegistry } from "../objects/typeRegistry";
 import { RuntimeContext } from "../runtime/runtimeContext";
 import { Tokens } from "../runtime/tokens";
 import { defineGlobal } from "./globalProvider";
@@ -36,63 +35,61 @@ class SantaiEnvMap extends SantaiObject {
 }
 
 defineGlobal("sistem", (serviceContainer) => {
-  return TypeRegistry.registerType(
-    new (class extends SantaiObject {
-      override readonly typeName: string = "sistem";
+  return new (class extends SantaiObject {
+    override readonly typeName: string = "sistem";
 
-      private readonly _argumen: SantaiObject;
-      private readonly _exit: SantaiObject;
-      private readonly _env: SantaiEnvMap;
+    private readonly _argumen: SantaiObject;
+    private readonly _exit: SantaiObject;
+    private readonly _env: SantaiEnvMap;
 
-      constructor(private readonly ctx: RuntimeContext) {
-        super(SantaiType.kBuiltinClass);
-        this._argumen = Factory.NewList(
-          ctx.args.map((arg) => Factory.NewString(arg))
-        );
-        this._exit = Factory.NewBuiltinFunction(
-          "keluar",
-          (_self, args) => {
-            const exitCode = args[0].isNumber() ? Math.trunc(args[0].value) : 0;
-            process.exit(exitCode);
-          },
-          undefined,
-          [optional("kode", Factory.NewNumber(0))]
-        );
-        this._env = TypeRegistry.registerType(new SantaiEnvMap());
+    constructor(private readonly ctx: RuntimeContext) {
+      super(SantaiType.kBuiltinClass);
+      this._argumen = Factory.NewList(
+        ctx.args.map((arg) => Factory.NewString(arg))
+      );
+      this._exit = Factory.NewBuiltinFunction(
+        "keluar",
+        (_self, args) => {
+          const exitCode = args[0].isNumber() ? Math.trunc(args[0].value) : 0;
+          process.exit(exitCode);
+        },
+        undefined,
+        [optional("kode", Factory.NewNumber(0))]
+      );
+      this._env = new SantaiEnvMap();
+    }
+
+    override getProperty(name: string): SantaiObject | undefined {
+      switch (name) {
+        case "versi":
+          return Factory.NewString(meta.VERSION_STRING);
+        case "nama":
+          return Factory.NewString(meta.LANG_NAME);
+        case "platform":
+          return Factory.NewString(process.platform);
+        case "direktori_kerja":
+          return Factory.NewString(process.cwd());
+        case "jalur_eksekusi":
+          return Factory.NewString(this.ctx.execPath);
+        case "nodejs":
+          return Factory.NewString(this.ctx.node);
+        case "argumen":
+          return this._argumen;
+        case "env":
+          return this._env;
+        case "keluar":
+          return this._exit;
+        default:
+          return undefined;
       }
+    }
 
-      override getProperty(name: string): SantaiObject | undefined {
-        switch (name) {
-          case "versi":
-            return Factory.NewString(meta.VERSION_STRING);
-          case "nama":
-            return Factory.NewString(meta.LANG_NAME);
-          case "platform":
-            return Factory.NewString(process.platform);
-          case "direktori_kerja":
-            return Factory.NewString(process.cwd());
-          case "jalur_eksekusi":
-            return Factory.NewString(this.ctx.execPath);
-          case "nodejs":
-            return Factory.NewString(this.ctx.node);
-          case "argumen":
-            return this._argumen;
-          case "env":
-            return this._env;
-          case "keluar":
-            return this._exit;
-          default:
-            return undefined;
-        }
-      }
+    override isTruthy(): boolean {
+      return true;
+    }
 
-      override isTruthy(): boolean {
-        return true;
-      }
-
-      override inspect(): string {
-        return `<sistem ${meta.LANG_NAME} ${meta.VERSION_STRING}>`;
-      }
-    })(serviceContainer.get(Tokens.RuntimeContext))
-  );
+    override inspect(): string {
+      return `<sistem ${meta.LANG_NAME} ${meta.VERSION_STRING}>`;
+    }
+  })(serviceContainer.get(Tokens.RuntimeContext));
 });
