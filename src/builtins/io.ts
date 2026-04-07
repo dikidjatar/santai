81177d49b4e9154d91a15889081e4e84ff7c21f6
dikidjatar <dikidjatar@gmail.com>
2026-04-07 20:53:41 +0700

    feat(builtins): add global builtin function baca

--- src/builtins/io.ts
+++ src/builtins/io.ts
@@ -1,9 +1,11 @@
 // Copyright (c) [2026] [Diki Djatar]
 // SPDX-License-Identifier: MIT
 
+import fs from "fs";
 import { writeLineToStdout, writeToStdout } from "../base/output";
 import { Factory, SantaiObject } from "../objects/object";
 import { defineGlobalFunction } from "./builtin";
+import { optional } from "./paramSpec";
 
 function joinArgs(args: SantaiObject[]): string {
   return args.map((a) => a.inspect()).join(" ");
@@ -20,3 +22,16 @@ defineGlobalFunction("spil", (_self, args) => {
   writeLineToStdout(joinArgs(args));
   return Factory.Kosong;
 });
+
+// Simple implementation for input
+defineGlobalFunction(
+  "baca",
+  (_, args) => {
+    const prompt = args[0].inspect();
+    if (prompt) writeToStdout(prompt);
+    const buf = Buffer.alloc(1024);
+    const n = fs.readSync(0, buf, 0, buf.length, null);
+    return Factory.NewString(buf.subarray(0, n).toString().trimEnd());
+  },
+  [optional("pesan", Factory.NewString(""))]
+);
