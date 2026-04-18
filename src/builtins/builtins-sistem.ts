8eeaacfe640c611fbd0258eeffcda58794a7923c
dikidjatar <dikidjatar@gmail.com>
2026-04-18 14:04:25 +0700

    refactor(builtins-sistem): refactor add new builtins method

--- src/builtins/builtins-sistem.ts
+++ src/builtins/builtins-sistem.ts
@@ -3,92 +3,116 @@
 
 import * as meta from "../base/meta";
 import { isUndefined } from "../base/types";
-import { Factory, SantaiObject } from "../objects/object";
+import {
+  BuiltinFunction,
+  CallSite,
+  Factory,
+  GlobalMethodParam,
+  SantaiObject,
+} from "../objects/object";
+import { ObjectUtil } from "../objects/object-util";
+import { registerPropertyProvider } from "../objects/propertyRegistry";
+import { coerceToString } from "../objects/protocol";
+import { SpecialName } from "../objects/specialNames";
 import { SantaiType } from "../objects/st-type";
 import { RuntimeContext } from "../runtime/runtimeContext";
 import { Tokens } from "../runtime/tokens";
+import { asGetter, getNumber, mapParams } from "./builtin-util";
 import { defineGlobal } from "./globalProvider";
-import { optional } from "./paramSpec";
+import { optional, required } from "./paramSpec";
 
-class SantaiEnvMap extends SantaiObject {
-  override readonly typeName: string = "env";
+class STSystem extends SantaiObject {
+  override readonly typeName: string = "sistem";
+  private readonly _properties: Map<string, SantaiObject> = new Map();
 
-  constructor() {
-    super(SantaiType.kBuiltinClass);
+  get properties(): [string, SantaiObject][] {
+    return Array.from(this._properties.entries());
+  }
+
+  get propertyNames(): string[] {
+    return [...this._properties.keys()];
+  }
+
+  constructor(readonly context: RuntimeContext) {
+    super(SantaiType.kSystem);
+    this._properties.set("versi", Factory.NewString(meta.VERSION_STRING));
+    this._properties.set("nama", Factory.NewString(meta.LANG_NAME));
+    this._properties.set("platform", Factory.NewString(process.platform));
+    this._properties.set("direktori_kerja", Factory.NewString(process.cwd()));
+    this._properties.set("jalur_eksekusi", Factory.NewString(context.execPath));
+    this._properties.set("nodejs", Factory.NewString(context.node));
+    this._properties.set("arsitektur", Factory.NewString(process.arch));
+    this._properties.set(
+      "argumen",
+      Factory.NewList(context.args.map((arg) => Factory.NewString(arg)))
+    );
+    const envs = Object.entries(process.env).map(([key, value]) =>
+      Factory.NewPair(key, value ? Factory.NewString(value) : Factory.Kosong)
+    );
+    this._properties.set("env", Factory.NewMap(envs));
   }
 
   override getProperty(name: string): SantaiObject | undefined {
-    const value = process.env[name];
-    return isUndefined(value) ? Factory.Kosong : Factory.NewString(value);
-  }
-
-  override dir(): readonly string[] {
-    return Object.keys(process.env);
-  }
-
-  override isTruthy(): boolean {
-    return true;
-  }
-
-  override inspect(): string {
-    const entries = Object.entries(process.env)
-      .map(([k, v]) => `${k}="${v ?? ""}"`)
-      .join(", ");
-    return `env { ${entries} }`;
+    const property = this._properties.get(name);
+    if (!isUndefined(property)) return property;
+    return super.getProperty(name);
   }
 }
 
-defineGlobal("sistem", (serviceContainer) => {
-  return new (class extends SantaiObject {
-    override readonly typeName: string = "sistem";
+function method(
+  name: string,
+  fn: (
+    callsite: CallSite,
+    self: STSystem,
+    ...args: SantaiObject[]
+  ) => SantaiObject,
+  params?: GlobalMethodParam[]
+): BuiltinFunction {
+  return Factory.NewBuiltinFunction(
+    name,
+    ObjectUtil.wrapMethod({
+      fn,
+      assertDescriptor: (callsite, self) =>
+        ObjectUtil.checkObjectDescriptor(
+          callsite,
+          self,
+          SantaiType.kSystem,
+          "sistem"
+        ),
+    }),
+    undefined,
+    [required("gue"), ...(params ? params : [])]
+  );
+}
 
-    private readonly _properties: Map<string, SantaiObject> = new Map();
+const systemMethods: BuiltinFunction[] = [
+  method(SpecialName.__teks__, (callsite, self) => {
+    const entries = self.properties
+      .map(([key, val]) => `${key}="${coerceToString(callsite, val) ?? ""}"`)
+      .join(", ");
+    return Factory.NewString(`sistem { ${entries} }`);
+  }),
+  method(SpecialName.__logika__, () => Factory.True),
+  method(SpecialName.__daftarproperti__, (_, self) => {
+    return Factory.NewList(
+      [...self.propertyNames, ...mapParams(systemMethods)].map((name) =>
+        Factory.NewString(name)
+      )
+    );
+  }),
+  method("keluar", (_, __, code) => process.exit(getNumber(code)), [
+    optional("kode", Factory.NewNumber(0)),
+  ]),
+];
 
-    constructor(ctx: RuntimeContext) {
-      super(SantaiType.kBuiltinClass);
+registerPropertyProvider(
+  SantaiType.kSystem,
+  asGetter(systemMethods),
+  mapParams(systemMethods)
+);
 
-      const exit = Factory.NewBuiltinFunction(
-        "keluar",
-        (_self, args) => {
-          const exitCode = args[0].isNumber() ? Math.trunc(args[0].value) : 0;
-          process.exit(exitCode);
-        },
-        undefined,
-        [optional("kode", Factory.NewNumber(0))]
-      );
-
-      this.set("versi", Factory.NewString(meta.VERSION_STRING));
-      this.set("nama", Factory.NewString(meta.LANG_NAME));
-      this.set("platform", Factory.NewString(process.platform));
-      this.set("direktori_kerja", Factory.NewString(process.cwd()));
-      this.set("jalur_eksekusi", Factory.NewString(ctx.execPath));
-      this.set("nodejs", Factory.NewString(ctx.node));
-      this.set(
-        "argumen",
-        Factory.NewList(ctx.args.map((arg) => Factory.NewString(arg)))
-      );
-      this.set("env", new SantaiEnvMap());
-      this.set("keluar", exit);
-    }
-
-    private set(name: string, value: SantaiObject): void {
-      this._properties.set(name, value);
-    }
-
-    override getProperty(name: string): SantaiObject | undefined {
-      return this._properties.get(name);
-    }
-
-    override dir(): readonly string[] {
-      return [...this._properties.keys()].sort();
-    }
-
-    override isTruthy(): boolean {
-      return true;
-    }
-
-    override inspect(): string {
-      return `<sistem ${meta.LANG_NAME} ${meta.VERSION_STRING}>`;
-    }
-  })(serviceContainer.get(Tokens.RuntimeContext));
-});
+defineGlobal(
+  "sistem",
+  (serviceContainer) =>
+    new STSystem(serviceContainer.get(Tokens.RuntimeContext))
+);
